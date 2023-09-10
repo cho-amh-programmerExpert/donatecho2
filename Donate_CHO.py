cdafb8494971131682861d744da78bbd585334cf
amh-cho <102322216+cho-amh-programmerExpert@users.noreply.github.com>
2023-09-10 19:38:12 +0330

    Add files via upload

--- Donate_CHO.py
+++ Donate_CHO.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_extras.colored_header import colored_header
 from st_paywall import add_auth
 
-st.set_page_config(page_icon="🪙", page_title="Donate C.H.O | Payment Plan")
+st.set_page_config(page_icon="🪙", page_title="Donate C.H.O | Payment Plan", initial_sidebar_state="expanded")
 hidden_streamlit_style = """
                 <style>
                 #MainMenu {visibility: hidden;}
@@ -13,14 +13,16 @@ st.markdown(hidden_streamlit_style, unsafe_allow_html=True)
 
 # ====== Authentication & Subscription ====== #
 
+state = ":red[InActive]**"
 colored_header(label="💸 Donation Status Page", description="The state of donating to C.H.O", color_name="red-70")
+_, _, _, l, m, r, _, _, _ = st.columns(9)
+l.divider()
+m.write(f"**:blue[Status]: {state}")
+r.divider()
 add_auth(required=True)
+state = ":green[Active]**"
 
 # ====== Main App: After Subscription ====== #
 
 st.write("**Thank you so much for subscribing! Your help is very much appreciated.**")
-_, _, _, l, m, r, _, _, _ = st.columns(9)
-l.divider()
-m.write(f"**:blue[Status]: :green[Active]**")
-r.divider()
 st.write(f"**:orange[Subscribed Via]: \":green[{st.session_state.email}]\"**")
